81c027610f38170734ac57b63fb39bc76f56e497
mcrossla11 <Ymcrossla11@gmail.com>
2026-03-23 13:31:30 -0400

    Fix port for Railway deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all origins and parse incoming JSON request bodies
 app.use(cors());
